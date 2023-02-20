f7671e703fc6d026b28480314aef33fe315a9427
NithanB <pa.nithan_st@tni.ac.th>
2023-02-20 14:13:59 +0700

    Create proj.py

---
+++ proj.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = {'Weight-lifting':90,'Aerobics':165,'Skiing':285,'Rowing':210}
+
+x = list(data.keys())
+y = list(data.values())
+
+fig = plt.figure(figsize = (10,5))
+
+plt.bar(x,y, color = 'green', width = 0.5)
+
+plt.xlabel("Activities")
+plt.ylabel("Calories burned")
+plt.title("Activities vs Calories Burned!")
+plt.show()
