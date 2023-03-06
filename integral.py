26c3e5edf1313892c480bef568513cb8a82432b0
NithanB <pa.nithan_st@tni.ac.th>
2023-03-06 15:05:30 +0700

    Create integral.py

---
+++ integral.py
@@ -0,0 +1,35 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Polygon
+m = 17.633333333333333
+B= -356
+def func(x):
+    return m*x + B
+
+
+a, b = 50,  80 # integral limits
+x = np.linspace(20, 100)
+y = func(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y, 'r', linewidth=2)
+ax.set_ylim(bottom=0)
+
+# Make the shaded region
+ix = np.linspace(a, b)
+iy = func(ix)
+verts = [(a, 0), *zip(ix, iy), (b, 0)]
+poly = Polygon(verts, facecolor='0.9', edgecolor='0.5')
+ax.add_patch(poly)
+
+
+fig.text(0.9, 0.05, '$x$')
+fig.text(0.1, 0.9, '$y$')
+plt.xlabel('Time')
+plt.ylabel('Calories burned')
+ax.spines[['top', 'right']].set_visible(False)
+ax.set_xticks([a, b], labels=[a, b])
+ax.set_yticks([])
+plt.title('Exercise Graph ' + "y = " + str(m) + "x + " + str(B), fontsize = 10)
+plt.show()
